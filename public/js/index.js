b0d5758b6a6c908607630e19bd44cbd01151bd3c
Francois Greeff <francois.greeff@bglgroup.co.uk>
2019-01-17 16:22:54 +0000

    Greet new user and alert on new connection

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,9 @@ var socket = io()
 
 socket.on('connect', function() {
   console.log('Connected to server')
-  document.write('Connected to server')
 })
 
 socket.on('disconnect', function() {
-  document.write('Disconnected from server')
   console.log('Disconnected from server')
 })
 
--- server/server.js
+++ server/server.js
@@ -18,8 +18,20 @@ app.use(express.static(publicPath))
 io.on('connection', socket => {
   console.log('New user connected')
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  })
+
+  //broadcast call
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  })
+
   socket.on('createMessage', message => {
-    console.log('createMessage: server got your message', message)
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
